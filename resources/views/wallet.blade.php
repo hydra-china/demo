e043111efd70569bda33261cc5e03ca869262bce
phamquanglinhdev <Phamquanglinhdev@gmail.com>
2024-03-10 12:45:15 +0700

    f

--- resources/views/wallet.blade.php
+++ resources/views/wallet.blade.php
@@ -52,7 +52,7 @@
         <div class="modal-dialog modal-dialog-centered">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="exampleModalLabel">Rút tiền thất bại</h1>
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Rút tiền</h1>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
@@ -84,7 +84,7 @@
         <div class="modal-dialog modal-dialog-centered">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="exampleModalLabel">Rút tiền thất bại</h1>
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Rút tiền</h1>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
